4365138f7be4c99945488a4f3be81a838844b56e
marti <1240690@gmail.com>
2026-03-20 15:01:59 +0000

    Initial commit: Adicionada a estrutura base da REST API

---
+++ psoft_aisafe/src/main/java/com/example/psoft_aisafe/PsoftAisafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.psoft_aisafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsoftAisafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsoftAisafeApplication.class, args);
+	}
+
+}
---
+++ psoft_aisafe/src/test/java/com/example/psoft_aisafe/PsoftAisafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psoft_aisafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsoftAisafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
